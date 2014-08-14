3c74ea21f959eb67048152bc49ab0b9ff13dd15c
Stephen Wilkinson <du_monde@hotmail.com>
2014-08-14 15:05:46 +0000

    [Blog] Added basic page load tests for front controller

---
+++ Tests/Controller/FrontControllerTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace StemsBlogBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class FrontControllerTest extends WebTestCase
+{
+	protected $client;
+
+	public function __construct() 
+	{
+		$this->client = static::createClient();
+	}
+
+    public function testList()
+    {
+    	// load the page
+        $crawler = $this->client->request('GET', '/magazine');
+
+        // check any content loaded
+        $this->assertTrue($crawler->filter('.cms-page')->count() > 0);
+
+        // optional test cases based on restful or paginated loading
+        if ($this->assertTrue($crawler->filter('.rest-load-more')->count() > 0)) {
+
+        }
+    }
+
+    public function testPost()
+    {
+    	// load the page for the first ever blog post
+        $crawler = $this->client->request('GET', '/the-wedding-guest-edit');
+
+        // check any content loaded
+        $this->assertTrue($crawler->filter('.cms-page')->count() > 0);
+    }
+}
