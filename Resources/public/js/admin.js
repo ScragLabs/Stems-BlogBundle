53f264ceb5c45badf8dff8723c528ad6ddf42e78
Stephen Wilkinson <du_monde@hotmail.com>
2014-09-03 22:33:44 +0000

    Moved bundle-specific admin js into public

---
+++ Resources/public/js/admin.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+
+	/**
+	 * Update the blog slug
+	 */
+	var admin_post_type_slug = $('#admin_post_type_slug');
+	var admin_post_type_excerpt = $('#admin_post_type_excerpt');
+	var admin_post_type_title = $('#admin_post_type_title');
+	
+	function updateBlogPostSlug() {
+		admin_post_type_slug.val(slugify(admin_post_type_title.val()+' '+admin_post_type_excerpt.val()));
+	}
+
+	admin_post_type_title.on('keyup', function(e){
+		updateBlogPostSlug();
+	});
+
+	admin_post_type_excerpt.on('keyup', function(e){
+		updateBlogPostSlug();
+	});
+
+	/** 
+	 * Add section to blog post
+	 */
+	$('.available-sections').on('click', 'a', function(e){
+		e.preventDefault();
+
+		$.get('/admin/blog/rest/add-section-type/'+$(this).data('type-id')).done(function(data) {
+			$('ul.sections').append('<li>'+data.html+'</li>');
+		});
+	});
+
+	/** 
+	 * Remove section from blog post
+	 */
+	$('.admin-editor').on('click', 'button.remove-section',  function(e){
+		e.preventDefault();
+		var button = $(this);
+		confirm('Are you sure you want to delete this section?');
+
+		$.get('/admin/blog/rest/remove-section/'+button.data('id')).done(function(data) {
+			if (data.status == 'success') {
+				button.parent().remove();
+			} else {
+				createFlashMessage(data.status, data.message);
+			}
+		});
+	});
+
+});
